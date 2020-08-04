841730da28ddbc8a2025b9f498890e768de6c10d
藤岡 <>
2020-08-04 13:58:50 +0900

    express staticのパス変更

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ server = app.listen(process.env.PORT || 8080,function(){
   console.log(__dirname)
 });
 
-app.use(express.static(__dirname + '../src'));
+app.use(express.static('/src'));
 
 io = socket(server);
 
